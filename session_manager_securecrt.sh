de5f74675133f16d96b313a06679f1bbdf53ae53
rvdvelpe <ruud.vandervelpen@gmail.com>
2016-06-10 13:59:37 +0200

    some changes

--- session_manager_securecrt.sh
+++ session_manager_securecrt.sh
@@ -13,9 +13,12 @@ do
   ((n++))
 done
 
-#Read the session to connect to
-read -p "Which session do you want to connect to: " session
-session=${output[$session]}
+if [ ${n} = 1 ]; then
+    session=${output[0]}
+else
+    read -p "Which session do you want to connect to: " session
+    session=${output[$session]}
+fi
 
 #Get the variables to connect to
 username=`grep \"Username\" "${session}" | cut -d'=' -f 2`
@@ -24,6 +27,7 @@ hostname=`grep \"Hostname\" "${session}" | cut -d'=' -f 2`
 
 case "$protocol" in
 "SSH2")
+    echo "ssh $username@$hostname"
     ssh $username@$hostname
     ;;
 "TELNET")
