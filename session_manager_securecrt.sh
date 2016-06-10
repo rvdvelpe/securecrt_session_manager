e8c1b02748a13759f3bd6d289adedc2823e9b7b3
rvdvelpe <ruud.vandervelpen@gmail.com>
2016-06-10 13:20:16 +0200

    first commit

---
+++ session_manager_securecrt.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+read -p "Session name search: " search
+
+#Search all the session in securecrt with the search string (case insensitive)
+n=0;IFS=$'\n';for f in $(find $HOME/.vandyke/SecureCRT/Config/Sessions -iname "*$search*.ini"); do output[$n]=$f && ((n++)); done;
+
+#Print the output
+n=0
+for each in "${output[@]}"
+do
+  echo "$n : $each"
+  ((n++))
+done
+
+#Read the session to connect to
+read -p "Which session do you want to connect to: " session
+session=${output[$session]}
+
+#Get the variables to connect to
+username=`grep \"Username\" "${session}" | cut -d'=' -f 2`
+protocol=`grep \"Protocol\ Name\" "${session}" | cut -d'=' -f 2`
+hostname=`grep \"Hostname\" "${session}" | cut -d'=' -f 2`
+
+case "$protocol" in
+"SSH2")
+    ssh $username@$hostname
+    ;;
+"TELNET")
+    echo "code not yet written"
+    ;;
+*)
+    echo "protocol $protocol not know yet"
+    ;;
+esac
+
+exit
